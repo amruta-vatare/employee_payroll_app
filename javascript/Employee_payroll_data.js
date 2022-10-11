5d76bf00de8b4e445804858f00af14a84cb10e66
amruta.vatare <vatareamruta615@gmail.com>
2022-10-11 13:54:47 +0530

    modifed an employee_payroll_data implemented an getter setter for properties

--- javascript/Employee_payroll_data.js
+++ javascript/Employee_payroll_data.js
@@ -1,21 +1,12 @@
 class EmployeePayrollData {
-    id;
-    salary;
-    gender;
-    startDate;
-
-    constructor(...params) {
-        this.id = params[0];
-        this.name = params[1];
-        this.salary = params[2];
-        this.gender = params[3];
-        this.startDate = params[4];
+    _id;
+    get id() {return this._id;}
+    set id(value) {
+        this._id = value;
     }
-
+    
     _name;
-    get name() {
-        return this._name;
-    }
+    get name() { return this._name; }
     set name(value) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
         if (nameRegex.test(value))
@@ -23,6 +14,42 @@ class EmployeePayrollData {
         else
             throw 'Name is Incorrect';
     }
+    _profilePic;
+    get profilePic() {return this._profilePic;}
+    set profilePic(value) {
+        this._profilePic = value;
+    }
+
+    _gender;
+    get gender() {return this._gender;}
+    set gender(value) {
+        this._gender = value;
+    }
+
+    _department;
+    get department() {return this._department;}
+    set department(value) {
+        this._department = value;
+    }
+
+    _salary;
+    get salary() {return this._salary;}
+    set salary(value) {
+        this._salary = value;
+    }
+
+    _note;
+    get note() {return this._note;}
+    set note(value) {
+        this._note = value;
+    }
+
+    _startDate;
+    get startDate() {return this._startDate;}
+    set startDate(value) {
+        this._startDate = value;
+    }
+
 
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
